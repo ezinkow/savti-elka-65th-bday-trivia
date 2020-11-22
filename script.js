45165cd9701f0651f132d310211ae2a97704833a
Elan Zinkow <ezinkow@gmail.com>
2020-11-22 16:40:03 -0600

    new updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var currentQuestionIndex = -1
 var introButton = document.querySelector(".introBtn")
 
 $(".introBtn").on("click",function(){
-    $("#nextBtn").removeClass("hide")
+    $(".btnDiv").removeClass("hide")
     console.log("hello")
     currentQuestionIndex = currentQuestionIndex + 1
     $(".currentQuestion").html(questions[currentQuestionIndex])
@@ -18,8 +18,7 @@ $("#nextBtn").on("click",function() {
 
 var questions = [
     'this is question 1',
-    'this is question 2',
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/y2f1L7TkiUE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+    'this is question 2',' this is question 3, a video question! <br> <iframe width="560" height="315" src="https://www.youtube.com/embed/y2f1L7TkiUE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     'this is the text for previous video question',
 
 
