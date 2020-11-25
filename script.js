3a7e1561a7209f3e93e58247b449ed54aaed66fc
Elan Zinkow <ezinkow@gmail.com>
2020-11-25 17:27:18 -0600

    fixed gifs

--- script.js
+++ script.js
@@ -21,6 +21,24 @@ $(".backBtn").on("click",function() {
     $(".currentQuestion").html(questions[currentQuestionIndex])
 })
 
+
+
+$(".img-container").on("click", function(event) {
+  var element = event.target;
+
+  if (element.matches("img")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "still") {
+      element.setAttribute("data-state", "animate");
+      element.setAttribute("src", element.getAttribute("data-animate"));
+    } else if (state === "animate") {
+      element.setAttribute("data-state", "still");
+      element.setAttribute("src", element.getAttribute("data-still"));
+    }
+  }
+});
+
 var questions = [
     'this is question 1',
     'this is question 2',' this is question 3, a video question! <br> <iframe width="560" height="315" src="https://www.youtube.com/embed/y2f1L7TkiUE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
